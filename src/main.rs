ba666a7e58e8a056eb3d43d4d2cc4da3ed9accdd
Noah Dahlman <noah.dahlman@outlook.com>
2023-02-18 22:01:18 -0800

    tests: mocked tickers and financials

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod polygon;
 use polygon::api::{Polygon, API};
+use polygon::common::ResponseObject;
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv().ok();
-
-    Polygon::connect(std::env::var("POLYGON_API_KEY")?)
-        .await?
-        .markets()
-        .await?;
+    let markets = Polygon::new().await?.markets().await?;
+    let market_str: String = markets.into();
+    println!("{}", market_str);
     Ok(())
 }
